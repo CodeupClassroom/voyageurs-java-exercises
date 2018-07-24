287883a116e7ccc499bffb15792e8dc1fd5a8580
Justin Reich <justinreich.dev@gmail.com>
2018-07-24 09:58:31 -0500

    Add exercise solution for Syntax lesson

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,93 @@
 public class HelloWorld {
 
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+
+//        Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
+
+//        int myFavoriteNumber;
+//        myFavoriteNumber = 7;
+//        System.out.println(myFavoriteNumber);
+
+//        Create a String variable named myString and assign a string value to it, then print the variable out to the console.
+
+//        String myString = "Justin";
+//        System.out.println(myString);
+
+//                Change your code to assign a character value to myString. What do you notice?
+
+//        myString = "not Justin";
+//
+//        System.out.println(myString);
+//
+//        Change your code to assign the value 3.14159 to myString. What happens?
+
+
+//
+//        Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
+//
+//        long myNumber;
+
+
+//        Change your code to assign the value 3.14 to myNumber. What do you notice?
+
+//        myNumber = 3.14;
+//        Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
+//        myNumber = 3000000000L;
+//        System.out.println(myNumber);
+
+//
+//        Change your code to assign the value 123 to myNumber.
+
+
+//        Why does assigning the value 3.14 to a variable declared as a long not compile, but assigning an integer value does?
+
+
+//                Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this?
+//
+//        float myNumber = (float) 3.14;
+
+//                Copy and paste the following code blocks one at a time and execute them
+//
+//
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        What is the difference between the above code blocks? Explain why the code outputs what it does.
+//
+//                Try to create a variable named class. What happens?
+
+
+//        Rewrite the following expressions using the relevant shorthand assignment operators:
+//
+//
+//        int x = 4;
+////        x = x + 5;
+//        x += 5;
+
+//
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+//
+//        int x = 10;
+//        int y = 2;
+
+//        x = x / y;
+//        x /= y;
+//        y -= x;
+//        System.out.println(x);
+//        System.out.println(y);
+
+
+
     }
+
 
 }
