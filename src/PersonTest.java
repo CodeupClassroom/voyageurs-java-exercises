9959919b049208792e76057c9b50a8fcc7d6e109
fer <fernandoza@gmail.com>
2018-07-30 16:26:53 -0500

    feat: add solution to the first part

---
+++ src/PersonTest.java
@@ -0,0 +1,14 @@
+public class PersonTest {
+
+    public static void main(String[] args) {
+        Person human = new Person();
+        human.setName("fer");
+        human.sayHello();
+
+        Person employee = new Person();
+        employee.setName("Phillip");
+        employee.sayHello();
+
+    }
+
+}
