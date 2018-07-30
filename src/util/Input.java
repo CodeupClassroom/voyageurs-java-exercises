d1bea0c0e024891c324ab9570abaf6f66e19381c
fer <fernandoza@gmail.com>
2018-07-30 17:02:51 -0500

    feat: add comments

--- src/util/Input.java
+++ src/util/Input.java
@@ -6,15 +6,18 @@ public class Input {
 
     private Scanner sc = new Scanner(System.in);
 
+    // Original version with params
     public String getString(String prompt){
         System.out.println(prompt);
         return this.sc.nextLine().toLowerCase().trim();
     }
 
+    // Another version without params
     public String getString(){
         return this.getString("Type: ");
     }
 
+    // YesNo reuses some code from getString
     public boolean yesNo(){
         String result = this.getString("Type yes or no (y/n)");
         return (result.equals("y") || result.equals("yes"));
