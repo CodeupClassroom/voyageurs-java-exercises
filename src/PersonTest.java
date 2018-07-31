d7fc156036c20676d41641ab90610c9dd940c90c
fer <fernandoza@gmail.com>
2018-07-31 14:46:58 -0500

    feat: finish input class

--- src/PersonTest.java
+++ src/PersonTest.java
@@ -4,11 +4,19 @@ public class PersonTest {
 
     public static void main(String[] args) {
 
-        Input input = new Input();
+//        Input class tests
 
-        System.out.println(input.getString());
-        System.out.println(input.getString("GIVE ME SOMETHING!"));
-        System.out.println(input.yesNo());
+//        Input input = new Input();
+//
+//        System.out.println(input.getString());
+//        System.out.println(input.getString("GIVE ME SOMETHING!"));
+//        System.out.println(input.yesNo());
+
+//        System.out.println(input.getInt());
+//        System.out.println(input.getInt(1,5));
+
+//        System.out.println(input.getDouble());
+//        System.out.println(input.getDouble(1.25,2.55));
 
 
         Person human = new Person();
