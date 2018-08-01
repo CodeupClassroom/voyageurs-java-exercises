89d3ca0e268620d510e0d222007235465788e2b0
fer <fernandoza@gmail.com>
2018-08-01 10:39:30 -0500

    feat: finish the main menu and the 1st 2 options with refactoring

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -10,34 +10,42 @@ public class MoviesApplication {
         Input input = new Input();
         Movie[] movies = MoviesArray.findAll();
 
-        System.out.println("What would you like to do?");
+        while(true){
 
-        System.out.println("0 - exit\n" +
-                "1 - view all movies\n" +
-                "2 - view movies in the animated category\n" +
-                "3 - view movies in the drama category\n" +
-                "4 - view movies in the horror category\n" +
-                "5 - view movies in the scifi category");
+            System.out.println("What would you like to do?");
 
-        switch (input.getInt(0, 5)){
-            case 0:
-                System.out.println("Bye");
-                System.exit(0);
-                break;
-            case 1:
-                for(Movie movie: movies){
-                    System.out.println("Title: "+ movie.getName() + " category: " + movie.getCategory());
-                }
-                break;
-            case 2:
-                for (Movie movie: movies) {
-                    if(movie.getCategory().equals("animated")){
-                        System.out.println("Title: "+ movie.getName() + " category: " + movie.getCategory());
+            System.out.println("0 - exit\n" +
+                    "1 - view all movies\n" +
+                    "2 - view movies in the animated category\n" +
+                    "3 - view movies in the drama category\n" +
+                    "4 - view movies in the horror category\n" +
+                    "5 - view movies in the scifi category");
+
+            switch (input.getInt(0, 5)){
+                case 0:
+                    System.out.println("Bye");
+                    System.exit(0);
+                    break;
+                case 1:
+                    for(Movie movie: movies){
+                        printMovie(movie);
                     }
-                }
+                    break;
+                case 2:
+                    for (Movie movie: movies) {
+                        if(movie.getCategory().equals("animated")){
+                            printMovie(movie);
+                        }
+                    }
+            }
+
         }
 
 
-
     }
+
+    public static void printMovie(Movie movie){
+        System.out.println("Title: "+ movie.getName() + " category: " + movie.getCategory());
+    }
+
 }
