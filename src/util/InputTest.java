ea794485e725908b79715eced35bddeedb6b0a70
fer <fernandoza@gmail.com>
2018-08-06 16:55:56 -0500

    feat: add solution to the exceptions exercise

---
+++ src/util/InputTest.java
@@ -0,0 +1,12 @@
+package util;
+
+public class InputTest {
+    public static void main(String[] args) {
+        Input input = new Input();
+
+        System.out.println("before transform");
+        System.out.println(input.getInt());
+        System.out.println("Continue working");
+
+    }
+}
